48d68a1b23ed63a02eb6798474e47a36e8d6bac8
guxiaobai <sikuan.gu@gmail.com>
2015-06-22 18:00:18 +0800

    update

--- service.sh
+++ service.sh
@@ -2,6 +2,7 @@
 
 # Service
 brew cask install launchrocket
+brew install nginx
 brew install mysql
 brew install memcached
 brew install redis
--- software.sh
+++ software.sh
@@ -2,6 +2,7 @@
 
 # git command line tools
 brew install tig
+brew install wget
 
 # ImageMagick
 # 安裝 ImageMagick 需先有 X11 的 support,OSX 10.8 拿掉了...
@@ -32,10 +33,11 @@ brew cask install sequel-pro
 brew cask install xmind
 brew cask install android-file-transfer
 brew cask install dash
+brew cask install spectacle
+brew cask install the-unarchiver
 
 # baiduinput
 baiduinput=/opt/homebrew-cask/Caskroom/baiduinput/*/安装百度输入法.app
 if [ -d $baiduinput ]; then
   open $baiduinput
 fi
-
