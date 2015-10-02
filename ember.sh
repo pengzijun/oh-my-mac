dc7941aa323df86602aafb2d11ce715df0722d5e
guxiaobai <sikuan.gu@gmail.com>
2015-10-02 18:59:25 +0800

    add ember module

---
+++ ember.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+brew install node
+brew install watchman
+
+npm config set registry https://registry.npm.taobao.org
+
+npm install -g bower 
+npm install -g ember-cli
+# npm install -g phantomjs2
