d72db5a1646ebb2facfcb28952a2c006b7a1341f
lemon <lemon@lemons-MBP.lan>
2015-10-25 21:46:41 +0800

    modify ruby

--- ruby.sh
+++ ruby.sh
@@ -17,17 +17,15 @@ rbenv install -l
 echo -n "Please input ruby version: "
 read version
 mkdir -p $(rbenv root)/cache
-wget -c http://ruby.taobao.org/mirrors/ruby/ruby-${version}.tar.gz -O $(rbenv root)/cache/ruby-${version}.tar.gz
+
 export RUBY_CONFIGURE_OPTS="--disable-install-doc  --with-readline-dir=$(brew --prefix readline)"
 rbenv install -v ${version}
 rbenv global ${version}
 rbenv shell ${version}
 
 # Gem
-gem sources --remove https://rubygems.org/
+gem sources --remove https://rubygems.org
 gem sources -a https://ruby.taobao.org
-gem install rubygems-update -V -N
-update_rubygems
 gem install rails -V -N
 gem install powder -V -N
 
