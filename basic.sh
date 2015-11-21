384dab515d51a3ffeb7a30a5ead83f79b3a7730e
guxiaobai <sikuan.gu@gmail.com>
2015-11-21 13:06:51 +0800

    update oh-my-zsh

--- basic.sh
+++ basic.sh
@@ -2,14 +2,8 @@
 
 # Oh My Zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# 设置 history 时间格式
-sed -i '43s/^#[ \t]*\(HIST_STAMPS\).*/\1="yyyy-mm-dd"/' ~/.zshrc
-
-# 关闭自动更新
-sed -i '18s/^#[ \t]*\(DISABLE_AUTO_UPDATE\).*/\1="false"/' ~/.zshrc
-
-# command line 中文显示
+sed -i '' -e '43s/^#[ \t]*\(HIST_STAMPS\).*/\1="yyyy-mm-dd"/' ~/.zshrc
+sed -i '' -e '18s/^#[ \t]*\(DISABLE_AUTO_UPDATE\).*/\1="false"/' ~/.zshrc
 printf 'export LANG=en_US.UTF-8\n' >> ~/.zshrc
 printf 'export LC_ALL=en_US.UTF-8\n' >> ~/.zshrc
 
