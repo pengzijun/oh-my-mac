eb897aa39e6e63f4cf832b70599e939ee4f5d8a4
guxiaobai <sikuan.gu@gmail.com>
2016-02-27 20:39:28 -0800

    update ember

--- ember.sh
+++ ember.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+export NVM_DIR="/Users/lemon/.nvm"
+export NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/dist
 
 brew install watchman
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+nvm install node
+npm config set registry https://registry.npm.taobao.org
 npm install -g ember-cli
-# npm install -g phantomjs2
+npm install -g bower
+npm install -g phantomjs-prebuilt
